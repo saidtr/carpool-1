55b2bade2452fba05f2139b0daa091ea09e8eb4f
itaypk@gmail.com <itaypk@gmail.com@4e9e44c4-d5e9-a9f0-7f8e-a17831e018fd>
2010-09-22 14:44:02 +0000

    - Wild card support

--- Carpool/jobs/ShowInterestNotifier.php
+++ Carpool/jobs/ShowInterestNotifier.php
@@ -4,11 +4,11 @@ include '../public/env.php';
 include APP_PATH . '/Bootstrap.php';
 
 function buildIndexStr($from, $to) {
-	return $from . '-' . $to;
+	return ($from == LOCATION_DONT_CARE ? '*' : $from) . '-' . ($to == LOCATION_DONT_CARE ? '*' : $to);
 }
 
-function notify($rideId, $potentialRideId) {
-	Logger::debug(__METHOD__ . "($rideId, $potentialRideId)");
+function notifyAll($rideId, $potentialRideIds) {
+	Logger::debug(__METHOD__ . "($rideId, " . json_encode($potentialRideIds) . ")");
 }
 
 Logger::info('ShowInterestNotifier: started');
@@ -32,8 +32,23 @@ $allRides = $db->searchRides($searchParams);
 //var_dump($allRides);
 $rideIdx = array();
 foreach ($allRides as $ride) {
-	$index = buildIndexStr($ride['SrcCityId'], $ride['DestCityId']); 
-	$fromIdx[$index] = $ride['Id']; 
+	$index = buildIndexStr($ride['SrcCityId'], $ride['DestCityId']);
+	// Put two additional indexes to fit "wild card" search (location = everywhere)
+	$indexWildCardFrom = buildIndexStr(LOCATION_DONT_CARE, $ride['DestCityId']);
+	$indexWildCardTo = buildIndexStr($ride['SrcCityId'], LOCATION_DONT_CARE);
+	if (!isset($fromIdx[$index])) {
+		$fromIdx[$index] = array();
+	}
+	$fromIdx[$index] []= $ride['Id'];
+	if (!isset($fromIdx[$indexWildCardFrom])) {
+		$fromIdx[$indexWildCardFrom] = array();
+	}
+	$fromIdx[$indexWildCardFrom] []= $ride['Id'];
+	if (!isset($fromIdx[$indexWildCardTo])) {
+		$fromIdx[$indexWildCardTo] = array();
+	}
+	$fromIdx[$indexWildCardTo] []= $ride['Id'];
+	
 }
 var_dump($fromIdx);
 
@@ -41,10 +56,13 @@ $toNotifyRides = $db->searchRides(array('status' => STATUS_LOOKING));
 foreach ($toNotifyRides as $ride) {
 	$index = buildIndexStr($ride['SrcCityId'], $ride['DestCityId']); 
 	if (isset($fromIdx[$index])) {
-		notify($ride['Id'], $fromIdx[$index]);
+		notifyAll($ride['Id'], $fromIdx[$index]);
 	} 
 }
 
+// TODO: Handle "wildcard" (everything) - the current index solution seems 
+// like a bad way to do it 
+// TODO: Handle contact who wants more than a single ride
 // TODO: Update last run
 
 Logger::info('ShowInterestNotifier: done');
