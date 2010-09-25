a46f5298d0c2f3f408932bc7ab5f358d2ca53d69
itaypk <itaypk@4e9e44c4-d5e9-a9f0-7f8e-a17831e018fd>
2010-09-25 13:56:20 +0000

    - LastRun need a default value

--- Carpool/sql/create.sql
+++ Carpool/sql/create.sql
@@ -3,6 +3,7 @@ CREATE TABLE Cities (Id INTEGER PRIMARY KEY, Name TEXT);
 CREATE TABLE Contacts (Identifier TEXT, Email TEXT, Id INTEGER PRIMARY KEY, Name TEXT, Phone TEXT);
 CREATE TABLE Ride (TimeUpdated NUMERIC, TimeCreated NUMERIC, Comment TEXT, Status NUMERIC, TimeEvening NUMERIC, TimeMorning NUMERIC, ContactId NUMERIC, DestCityId NUMERIC, DestLocation TEXT, SrcCityId NUMERIC, SrcLocation TEXT, Id INTEGER PRIMARY KEY);
 CREATE TABLE ShowInterestNotifier (LastRun NUMERIC);
+INSERT INTO ShowInterestNotifier VALUES(0);
 CREATE INDEX RideDest ON Ride(DestCityId ASC);
 CREATE INDEX RideFrom ON Ride(SrcCityId ASC);
 CREATE UNIQUE INDEX Unique_City ON Cities(Name ASC);
