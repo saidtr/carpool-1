1468155d301633c2d8841b0ba4658bc7972832ff
itaypk <itaypk@4e9e44c4-d5e9-a9f0-7f8e-a17831e018fd>
2011-02-05 22:27:49 +0000

    Translations (in progress)

--- trunk/Carpool/public/cms/translations.php
+++ trunk/Carpool/public/cms/translations.php
@@ -5,11 +5,43 @@ include APP_PATH . "/Bootstrap.php";
 
 // This is a post - form submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $db = DatabaseHelper::getInstance();
+    $locales = LocaleManager::getInstance()->getLocales();
     
-    Utils::redirect('cms.php');
+    $toUpdate = array();
+    $toInsert = array();
+    foreach ($_POST as $key => $value) {
+        // Valid fields are in the form of Type_Id_Language. We are only interested in 'question' or 'questionnew'
+        if (!preg_match('/^(question|questionnew)_[0-9]+_' . LocaleManager::getDefaultLocale() . '/', $key)) {
+            continue;
+        }
+        
+        $keyParts = explode('_', $key);
+        $func = $keyParts[0];
+        $id = $keyParts[1];        // Id of the new/modified question
+        $qId = $keyParts[1];       // Id of the input element
+        
+        // We only need to add if there is some data in the "New question" field
+        $newQuestionAnswer = ($func === 'questionnew' && !Utils::isEmptyString($_POST['questionnew_0_' . LocaleManager::getDefaultLocale()]));
+        if ($newQuestionAnswer) {
+            $id = $db->getNextQuestionAnswerId();
+            $qId = 0;
+        } 
+        foreach ($locales as $lang) {
+            $question = isset($_POST[$func . '_' . $qId . '_' . $lang['Id']]) ? $_POST[$func . '_' . $qId . '_' . $lang['Id']] : null; 
+            $answer = isset($_POST[$func . '_' . $qId . '_' . $lang['Id']]) ? $_POST[$func . '_' . $qId . '_' . $lang['Id']] : null;
+            
+            if ($newQuestionAnswer) {
+                $db->insertQuestionAnswer($id, $lang['Id'], $question, $answer);
+            } elseif ($func === 'question') {
+                $db->updateQuestionAnswer($id, $lang['Id'], $question, $answer);
+            }
+            
+        }       
+    }   
+    
+    Utils::redirect('translations.php');
 } else {
-    
-    AuthHandler::putUserToken();
     
 ?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
@@ -28,8 +60,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <?php echo View_Header::render(_('Carpool CMS'))?>
 <div id="content">
 <h2><?php echo _('Questions and Answers editor')?></h2>
-<form id="qaDataForm" action="cms.php" method="post">
-	<table style="width: 100%">
+<form id="qaDataForm" action="translations.php" method="post">
+	<table style="width: 100%" summary="<?php echo _('Edit existing translations')?>">
 		<tr>
 			<th>
     			<span><?php echo _('Language') ?></span>
@@ -40,6 +72,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			<th>
     			<span><?php echo _('Answer') ?></span>
     		</th>		
+    		<th></th>
 		</tr>
 <?php 
 
@@ -48,6 +81,7 @@ $currentQuestions = DatabaseHelper::getInstance()->getQuestionsAnswers();
 
 foreach($currentQuestions as $questionAnswerAllLangs) {
     $id = $questionAnswerAllLangs[LocaleManager::getDefaultLocale()]['Id'];
+    $first = true;
     foreach ($locales as $lang => $locale) {
         $questionAnswer = isset($questionAnswerAllLangs[$lang]) ? $questionAnswerAllLangs[$lang] : null;
 ?>
@@ -56,11 +90,16 @@ foreach($currentQuestions as $questionAnswerAllLangs) {
     			<span><?php echo $locales[$lang]['Name'] ?></span>
     		</td>
         	<td>
-        		<input type="text" id="question_<?php echo $id ?>_<?php echo $lang ?>" value="<?php echo $questionAnswer['Question'] ?>" />
+        		<input type="text" id="question_<?php echo $id ?>_<?php echo $lang ?>" name="question_<?php echo $id ?>_<?php echo $lang ?>" value="<?php echo $questionAnswer['Question'] ?>" />
         	</td>
         	<td>
-        		<input type="text" id="answer_<?php echo $id ?>_<?php echo $lang ?>" value="<?php echo $questionAnswer['Answer'] ?>" />
+        		<input type="text" id="answer_<?php echo $id ?>_<?php echo $lang ?>" name="answer_<?php echo $id ?>_<?php echo $lang ?>" value="<?php echo $questionAnswer['Answer'] ?>" />
         	</td>
+        	<?php if ($first): ?>
+        	<td rowspan="<?php echo count($locales)?>">
+        		<input class="deleteButton" type="button" id="delete_<?php echo $id?>" value="Delete!" />
+        	</td>
+        	<?php $first = false; endif; ?>
     	</tr>
 <?php 
     } 
@@ -68,10 +107,7 @@ foreach($currentQuestions as $questionAnswerAllLangs) {
 ?>
 	</table>
 	<h2><?php echo _('Add new')?></h2>
-<?php 
-foreach ($locales as $lang => $locale) {
-?>	
-	<table style="width: 100%">
+	<table style="width: 100%" summary="<?php echo _('Add new translations')?>" >
 		<tr>
 			<th>
     			<span><?php echo _('Language') ?></span>
@@ -83,16 +119,19 @@ foreach ($locales as $lang => $locale) {
     			<span><?php echo _('Answer') ?></span>
     		</th>		
 		</tr>	
+<?php 
+foreach ($locales as $lang => $locale) {
+?>	
 	
     	<tr>
     		<td>
     			<span><?php echo $locales[$lang]['Name'] ?></span>
     		</td>
         	<td>
-        		<input type="text" id="question_<?php echo $questionAnswer['Id'] ?>_<?php echo $lang ?>" value="<?php echo $questionAnswer['Question'] ?>" />
+        		<input type="text" id="questionnew_0_<?php echo $lang ?>" name="questionnew_0_<?php echo $lang ?>" value="" />
         	</td>
         	<td>
-        		<input type="text" id="answer_<?php echo $questionAnswer['Id'] ?>_<?php echo $lang ?>" value="<?php echo $questionAnswer['Answer'] ?>" />
+        		<input type="text" id="answernew_0_<?php echo $lang ?>" name="answernew_0_<?php echo $lang ?>"  value="" />
         	</td>
     	</tr>
 	
@@ -101,7 +140,6 @@ foreach ($locales as $lang => $locale) {
 ?>	
 	
 	</table>
-	
 	<dl class="noFloat">
     	<dd>
     		<input type="submit" value="<?php echo _('Submit')?>" />
