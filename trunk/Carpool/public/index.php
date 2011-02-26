a34bc7821bdc584b78d422a46bf44d963e2c2e0b
itaypk <itaypk@4e9e44c4-d5e9-a9f0-7f8e-a17831e018fd>
2011-02-26 20:46:15 +0000

    Better show everything be default

--- trunk/Carpool/public/index.php
+++ trunk/Carpool/public/index.php
@@ -36,8 +36,8 @@ $availableSrcCities = $db->getCities();
 				<dd>
 					<label for="wantTo"><?php echo _('Show me')?>&nbsp;</label>
 					<select id="wantTo" name="wantTo">
-						<option value="<?php echo STATUS_DONT_CARE?>"><?php echo _('Everything')?></option>
-						<option value="<?php echo STATUS_OFFERED ?>" selected="selected" ><?php echo _('Available rides')?></option>
+						<option value="<?php echo STATUS_DONT_CARE?>" selected="selected"><?php echo _('Everything')?></option>
+						<option value="<?php echo STATUS_OFFERED ?>"><?php echo _('Available rides')?></option>
 						<option value="<?php echo STATUS_LOOKING  ?>"><?php echo _('People looking for a ride')?></option>
 					</select>
 				</dd>
