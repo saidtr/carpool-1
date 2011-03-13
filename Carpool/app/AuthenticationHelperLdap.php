36559d96b360409d33b21cc12f2250a64de7692f
itaypk <itaypk@4e9e44c4-d5e9-a9f0-7f8e-a17831e018fd>
2011-03-13 21:48:58 +0000

    Authentication helpers: used to support various authentication options

---
+++ Carpool/app/AuthenticationHelperLdap.php
@@ -0,0 +1,54 @@
+<?php
+
+class AuthenticationHelperLdap implements IAuthenticationHelper {
+    
+    const LDAP_DEFAULT_PORT = 389;
+    
+    // LDAP relevant error codes
+    // For full list of LDAP errors, see first comment in http://il2.php.net/manual/en/function.ldap-errno.php
+    const LDAP_INAPPROPRIATE_AUTH  = 48;
+    const LDAP_INVALID_CREDENTIALS = 49;
+    
+    function authenticate($params) {       
+        assert(isset($params['user']) && isset($params['password']));
+        
+        $con = false;
+        if (($domain = getConfiguration('auth.ldap.domain', 'Unspecified domain')) !== false) {
+            $con = ldap_connect($domain, getConfiguration('auth.ldap.port', self::LDAP_DEFAULT_PORT));
+        }
+        if ($con === false) {
+            throw new Exception(__METHOD__ . ": Failed to connect to $domain");
+        }
+        
+        $user = $params['user'];
+        $pass = $params['password'];
+        
+        if (ldap_bind($con, $user, $pass)) {
+            // We're assuming that the email used is as the user name
+            $email = $email = Utils::buildEmail($user);
+            
+            // Fetch contact
+            $contact = DatabaseHelper::getInstance()->getContactByEmail($email);
+            if ($contact !== false) {
+                return $contact['Id'];    
+            } else {
+                // Contact is not in the database - we better create it
+                return DatabaseHelper::getInstance()->addContact('', '', $email);
+            }           
+        } else {
+            $errCode = ldap_errno($con);
+            if ($errCode == self::LDAP_INAPPROPRIATE_AUTH || $errCode == self::LDAP_INVALID_CREDENTIALS) {
+                // Invalid credentials - simply fail
+                return false;
+            }
+            
+            // Internal error
+            throw new Exception("AuthenticationHelperLdap: authenticate: " . ldap_err2str($errCode));
+        }
+        
+        ldap_unbind($con);
+        
+        return true;
+    }
+    
+}
---
+++ Carpool/app/AuthenticationHelperPassword.php
@@ -0,0 +1,18 @@
+<?php
+
+class AuthenticationHelperPassword implements IAuthenticationHelper {
+    
+    function init() {
+        return true;
+    }
+    
+    function authenticate($params) {
+        assert(isset($params['user']) && isset($params['pass']));
+        
+        $user = $params['user'];
+        $pass = $params['pass'];
+        
+        return true;
+    }
+    
+}
