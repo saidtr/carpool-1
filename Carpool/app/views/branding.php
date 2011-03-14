ea0bb3683d3873294c87367b2d914f4da83bddc9
itaypk <itaypk@4e9e44c4-d5e9-a9f0-7f8e-a17831e018fd>
2011-03-14 19:53:04 +0000

    - Easier branding

--- Carpool/app/views/branding.php
+++ Carpool/app/views/branding.php
@@ -1,3 +1,15 @@
+<style>
+/* Possibly, place special branding styles here */
+#navbar {
+	
+}
+#navbar li a, #navbar li a:visited {
+	
+}
+#langSelectorForm select {
+	
+}
+</style>
 <div id="branding">
     <img id="imgLogoLeft" src="img/Branding_Logo_Left.gif" alt="Logo right" />
     <img id="imgLogoRight" src="img/Branding_Logo_Right.gif" alt="Logo left" />
