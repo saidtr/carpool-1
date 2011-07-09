2651994dd7d1153c7f27790c8e38aaa19578f497
itaypk <itaypk@4e9e44c4-d5e9-a9f0-7f8e-a17831e018fd>
2011-07-09 15:54:31 +0000

    Hopefully, mails would now look a little better

--- Carpool/app/layouts/mail.php
+++ Carpool/app/layouts/mail.php
@@ -1,9 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title></title>
 <style type="text/css">
-h1 { font-size: xx-large; } 
+body {
+	font:13px/1.231 arial, helvetica, clean, sans-serif;
+}
+div, p { 
+	margin: 2px; 
+	padding: 0; 
+}
+h1 { 
+	font-size: 1.5em; 
+	border-bottom: 1px solid #c0c0c0;
+	margin: 2px 2px 5px 2px;
+	padding: 0 0 2px 0;
+} 
+h2 { 
+	font-size: 1.2em; 
+}
 </style>
 </head>
 <body>
