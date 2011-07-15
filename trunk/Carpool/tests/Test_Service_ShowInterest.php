2d7687ab9c2633c01682c4aa12b6a3a6f2448f8e
itaypk <itaypk@4e9e44c4-d5e9-a9f0-7f8e-a17831e018fd>
2011-07-15 18:30:22 +0000

    Show interest: do the required adjustments to match the new "sharing" status.

--- trunk/Carpool/tests/Test_Service_ShowInterest.php
+++ trunk/Carpool/tests/Test_Service_ShowInterest.php
@@ -42,22 +42,23 @@ class Test_Service_ShowInterest extends PHPUnit_TestCase {
         
         $ride1 = TestUtils::createSimpleRide(1, 2, STATUS_LOOKING);
         $ride2 = TestUtils::createSimpleRide(3, 4, STATUS_LOOKING);
+        $ride3 = TestUtils::createSimpleRide(3, 4, STATUS_OFFERED);
+        $ride4 = TestUtils::createSimpleRide(3, 4, STATUS_SHARING);
         
         // And one inactive
-        $ride3 = TestUtils::createSimpleRide(5, 6, STATUS_OFFERED);
-        DatabaseHelper::getInstance()->updateRideActive($ride3, RIDE_INACTIVE);
+        $ride5 = TestUtils::createSimpleRide(5, 6, STATUS_OFFERED);
+        DatabaseHelper::getInstance()->updateRideActive($ride5, RIDE_INACTIVE);
         
         // Grab all potential rides - we should now have both
         DatabaseHelper::getInstance()->updateLastShowInterestNotifier(1);
-        $potentials = Service_ShowInterest::findPotentialRides(STATUS_LOOKING);   
-        
-        $this->assertRidesContainIds($potentials, array($ride1, $ride2));
+        $potentials = Service_ShowInterest::findPotentialRides(STATUS_LOOKING);         
+        $this->assertRidesContainIds($potentials, array($ride3, $ride4));
         
         $potentials = Service_ShowInterest::findPotentialRides(STATUS_OFFERED);
-        $this->assertEquals(0, count($potentials));
+        $this->assertRidesContainIds($potentials, array($ride1, $ride2, $ride4));
 
-        $potentials = Service_ShowInterest::findPotentialRides(STATUS_LOOKING);       
-        $this->assertEquals(2, count($potentials));
+        $potentials = Service_ShowInterest::findPotentialRides(STATUS_SHARING);       
+        $this->assertRidesContainIds($potentials, array($ride1, $ride2, $ride3, $ride4));
      
         // Now let's update the last run and make sure those rides won't be counted
         DatabaseHelper::getInstance()->updateLastShowInterestNotifier(time() + 1000);
@@ -74,12 +75,16 @@ class Test_Service_ShowInterest extends PHPUnit_TestCase {
         $ride4 = TestUtils::createSimpleRide(3, 4, STATUS_OFFERED, 0);
         $ride5 = TestUtils::createSimpleRide(5, 6, STATUS_LOOKING, 0);
         DatabaseHelper::getInstance()->updateRideActive($ride5, RIDE_INACTIVE);
+        $ride6 = TestUtils::createSimpleRide(5, 6, STATUS_SHARING, 1);
         
         $toNotify = Service_ShowInterest::findRidesToNotify(STATUS_LOOKING);
         $this->assertRidesContainIds($toNotify, array($ride1));
         
         $toNotify = Service_ShowInterest::findRidesToNotify(STATUS_OFFERED);
-        $this->assertRidesContainIds($toNotify, array($ride3));               
+        $this->assertRidesContainIds($toNotify, array($ride3));       
+
+        $toNotify = Service_ShowInterest::findRidesToNotify(STATUS_SHARING);
+        $this->assertRidesContainIds($toNotify, array($ride6));               
     }
     
     /**
@@ -96,8 +101,8 @@ class Test_Service_ShowInterest extends PHPUnit_TestCase {
         $ride4 = TestUtils::createSimpleRide(3, 4, STATUS_OFFERED);
         $ride5 = TestUtils::createSimpleRide(1, 7, STATUS_OFFERED);
         
-        $potentials = Service_ShowInterest::findPotentialRides(STATUS_OFFERED);
         $toNotify = Service_ShowInterest::findRidesToNotify(STATUS_LOOKING);
+        $potentials = Service_ShowInterest::findPotentialRides(STATUS_LOOKING);
         $matching = Service_ShowInterest::searchForMatchingRides($potentials, $toNotify);
         
         $expectedResults = array (
@@ -109,33 +114,48 @@ class Test_Service_ShowInterest extends PHPUnit_TestCase {
         $ride6 = TestUtils::createSimpleRide(1, LOCATION_DONT_CARE, STATUS_LOOKING);
         $ride7 = TestUtils::createSimpleRide(LOCATION_DONT_CARE, 7, STATUS_LOOKING);
         
+        // One for the sharing
+        $ride8 = TestUtils::createSimpleRide(1, 2, STATUS_SHARING);
+        
         $toNotify = Service_ShowInterest::findRidesToNotify(STATUS_LOOKING);
+        $potentials = Service_ShowInterest::findPotentialRides(STATUS_LOOKING);
         $matching = Service_ShowInterest::searchForMatchingRides($potentials, $toNotify);
         
         // We should now have 3 results:
-        //   ride1: ride3
-        //   ride6: ride3, ride5
+        //   ride1: ride3, ride8
+        //   ride6: ride3, ride5, ride8
         //   ride7: ride5
         $expectedResults = array (
-            $ride1 => array($ride3),
-            $ride6 => array($ride3, $ride5),
+            $ride1 => array($ride3, $ride8),
+            $ride6 => array($ride3, $ride5, $ride8),
             $ride7 => array($ride5)
         );
         $this->assertMatchingResults($matching, $expectedResults, "TestSearchForMatchingRides: Test 2");
         
         // Now test the other way
-        $potentials = Service_ShowInterest::findPotentialRides(STATUS_LOOKING);
         $toNotify = Service_ShowInterest::findRidesToNotify(STATUS_OFFERED);
-        
+        $potentials = Service_ShowInterest::findPotentialRides(STATUS_OFFERED);
         $matching = Service_ShowInterest::searchForMatchingRides($potentials, $toNotify);
         
         // We should now have (wildcards in the rides to notify are not supported yet):
         //   ride3: ride1
         // TODO: Implement support for wildcards on the other way as well
         $expectedResults = array (
-            $ride3 => array($ride1)
+            $ride3 => array($ride1, $ride8)
         );
-        $this->assertMatchingResults($matching, $expectedResults, "TestSearchForMatchingRides: Test 3");    
+        $this->assertMatchingResults($matching, $expectedResults, "TestSearchForMatchingRides: Test 3");
+
+        // And what happens when they share
+        $toNotify = Service_ShowInterest::findRidesToNotify(STATUS_SHARING);
+        $potentials = Service_ShowInterest::findPotentialRides(STATUS_SHARING);
+        $matching = Service_ShowInterest::searchForMatchingRides($potentials, $toNotify);
+        
+        // We should now have the following results:
+        //   ride8: ride1, ride3
+        $expectedResults = array (
+            $ride8 => array($ride3, $ride1)
+        );
+        $this->assertMatchingResults($matching, $expectedResults, "TestSearchForMatchingRides: Test 4");
     }
     
     
